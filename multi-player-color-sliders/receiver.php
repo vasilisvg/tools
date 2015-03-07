f380fbbbbe0ac157a4571bf446757bc83094c12e
Vasilis van Gemert <github@vasilis.nl>
2015-03-07 10:17:28 +0100

    Added a bit of styling

--- multi-player-color-sliders/receiver.php
+++ multi-player-color-sliders/receiver.php
@@ -4,8 +4,14 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Receiving colours</title>
+	<link rel="stylesheet" href="http://fonts.typotheque.com/WF-023273-007830.css">
 	<style>
 html {
+	font: 111%/1.5  "Fedra Sans Screen 2", helvetica, arial, sans-serif;
+	text-rendering: optimizeLegibility;
+	-webkit-font-feature-settings: "kern" 1, "liga" on, "dlig" on;
+	   -moz-font-feature-settings: "kern" 1, "liga" on, "dlig" on;
+	        font-feature-settings: "kern" 1, "liga" on, "dlig" on;
 	height: 100%;
 }
 body {
