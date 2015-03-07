d9bb5460eb1d24b1c39402a0bab151a81cadaea3
Vasilis van Gemert <github@vasilis.nl>
2015-03-07 21:43:15 +0100

    fix

--- multi-player-color-sliders/color-in-color/index.php
+++ multi-player-color-sliders/color-in-color/index.php
@@ -170,7 +170,7 @@ while(i<$sl.length){
 		
 		var conn = peer.connect('<?php echo $teacher; ?>');
 		conn.on('open', function() {
-		  var theHTML = document.querySelector('div').outerHTML);
+		  var theHTML = document.querySelector('div').outerHTML;
 		  conn.send(theHTML);
 		});
 	}
