4f8f038dba64c54a9083f2f3e31f9f64d3447b59
Vasilis van Gemert <github@vasilis.nl>
2015-04-12 23:39:32 +0200

    https

--- multi-player-color-sliders/color-in-color/index.php
+++ multi-player-color-sliders/color-in-color/index.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Making a colour</title>
-	<link rel="stylesheet" href="http://fonts.typotheque.com/WF-023273-007830.css">
+	<link rel="stylesheet" href="//fonts.typotheque.com/WF-023273-007830.css">
 <style>
 html {
 	font: 111%/1.5  "Fedra Sans Screen 2", helvetica, arial, sans-serif;
