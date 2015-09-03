513ba2837e5aaa458d332ca0c0559538a2fd7598
Vasilis van Gemert <github@vasilis.nl>
2015-09-03 19:47:31 +0200

    make it safe

--- screens/index.php
+++ screens/index.php
@@ -82,9 +82,34 @@ article:focus {
 </head>
 <body>
 <?php 
+function GET($name=NULL, $value=false, $option="default")
+{
+    $option=false; // Old version depricated part
+    $content=(!empty($_GET[$name]) ? trim($_GET[$name]) : (!empty($value) && !is_array($value) ? trim($value) : false));
+    if(is_numeric($content))
+        return preg_replace("@([^0-9])@Ui", "", $content);
+    else if(is_bool($content))
+        return ($content?true:false);
+    else if(is_float($content))
+        return preg_replace("@([^0-9\,\.\+\-])@Ui", "", $content);
+    else if(is_string($content))
+    {
+        if(filter_var ($content, FILTER_VALIDATE_URL))
+            return $content;
+        else if(filter_var ($content, FILTER_VALIDATE_EMAIL))
+            return $content;
+        else if(filter_var ($content, FILTER_VALIDATE_IP))
+            return $content;
+        else if(filter_var ($content, FILTER_VALIDATE_FLOAT))
+            return $content;
+        else
+            return preg_replace("@([^a-zA-Z0-9\+\-\_\*\@\$\!\;\.\?\#\:\=\%\/\ ]+)@Ui", "", $content);
+    }
+    else false;
+}
 $url = 'https://vasilis.nl/';
 if ( isset($_GET["u"]) ) {
-	$url = $_GET["u"];
+	$url = GET('u');
 }
 ?>
 	<article tabindex="0">
