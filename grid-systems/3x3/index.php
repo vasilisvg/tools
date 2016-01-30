da5e1a1c76d3be80c95926379cf0dbfadcc1753a
Vasilis van Gemert <github@vasilis.nl>
2016-01-30 15:49:28 +0100

    No robots, please

--- grid-systems/3x3/index.php
+++ grid-systems/3x3/index.php
@@ -28,6 +28,7 @@ $dl = $_GET["dl"];
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="robots" content="noindex, nofollow">
 	<title>Place things in a 3x3 grid — Grid Systems</title>
 	<link rel="stylesheet" href="css.css">
 </head>
